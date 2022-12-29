86a9e458c641a38f5c858acd553670bbc5847314
razieltx <deyberbejarano97@gmail.com>
2022-12-29 02:26:24 -0500

    Main page layout completed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import {dirname, join} from 'path';
 import { fileURLToPath } from 'url';
+import morgan from 'morgan';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
