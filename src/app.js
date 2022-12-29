9a41b3bf9b62eadf5598179f26dbea0af599675b
razieltx <deyberbejarano97@gmail.com>
2022-12-29 01:30:51 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'public/index.html'));
+})
+
+export default app;
